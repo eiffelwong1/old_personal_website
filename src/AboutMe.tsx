5cc0ba7dce2cfa3adbb5ca6d6b12696c0950e670
Sing Wong <eiffel@Sings-MacBook-Pro.local>
2023-04-25 18:55:27 -0700

    added in simple timeline

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -1,5 +1,52 @@
 import './AboutMe.css'
 import ContactMe from './components/ContactMe'
+import Timeline from './components/Timeline';
+
+const experience = [
+    {
+        date: "Apr 2022 - Feb 2023",
+        title: "Software Engineer, Eightfold AI",
+        description:
+            "Worked on backend infrastructure that enables internal services that uses AWS lambda to overcome runtime limit. Coded, deployed, supported, and maintained the feature as a team. Designed and implement DB schema and data storage model, REST APIs for a future product. Assisted with escalations, on-call, and build duty. Having a great understanding of the code development and deployment cycles.",
+    },
+    {
+        date: "Sep 2020 - June 2022",
+        title: "University of California, San Diego",
+        description: "Master of Science in Computer Science and Engineering - GPA 3.75 - Mainly focused on ML and System architecture",
+    },
+    {
+        date: "Feb 2020 - Sep 2020",
+        title: "Software Engineer, Translucence Biosystems",
+        description:
+            "Created a distributed pipeline for better use of processing power from multiple machines, up hardware utilization from <20% to over 90% for a machine learning pipeline, which later became a product (https://www.translucencebio.com/products/stitchy).",
+    },
+    {
+        date: "Mar 2019 - Feb 2020",
+        title: "Junior Specialist (ML research), UCI CAIDM",
+        description:
+            "Developed and trained U-net classifiers for segmenting cancer cells within a series of MRI scans for the 2019 Kidney and Kidney Tumor Segmentation Challenge.",
+    },
+    {
+        date: "Jun 2019 - Aug 2019",
+        title: "Instructor, iD Tech",
+        description:
+            "Taught students about Python, convolutional neural networks, and reinforcement learning. Created additional teaching materials to provide an up-to-date learning experience.",
+    },
+    {
+        date: "Sep 2017 - Dec 2019",
+        title: "University of California, Irvine",
+        description: "Bachelor of Science in Computer Science - GPA 3.7",
+    },
+    {
+        date: "Sep 2014 - June 2017",
+        title: "Foothill College",
+        description: "Studied in Computer Science",
+    },{
+        date: "July 1997",
+        title: "Born in 🇭🇰",
+    }
+];
+
 
 function AboutMe() {
     return (
@@ -27,12 +74,9 @@ function AboutMe() {
             <div className="bio">
                 <h2>Bio</h2>
 
-                <div> title='Software Engineer @ Eightfold AI' year="Apr2022-Feb2023"</div >
-                <div> title='Student @ University of California, San Diego' year="June 2022"</div >
-                <div> title='Software Engineer @ Translucence Biosystems' year="Feb-Sep2020"</div >
-                <div> title='Junior Specialist (ML research) @ UCI Center for AI in Diagnostic Medicine' year="Mar2019-Feb2020"</div >
-                <div> title='Student @ University of California, Irvine' year="Dec 2019"</div >
-                <div> title='Instructor @ iD Tech' year="Jun-Aug 2019"</div >
+                <Timeline
+                    events={experience}
+                ></Timeline>
 
 
             </div>
---
+++ src/components/Timeline.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Timeline.css';
+
+interface ITimelineEvent {
+  date: string;
+  title: string;
+  description?: string;
+  type?: "work" | "education" | "personal";
+}
+
+interface ITimelineProps {
+  events: ITimelineEvent[];
+}
+
+const TimelineEvent: React.FC<ITimelineEvent> = ({ date, title, description, type }) => {
+  return (
+    <div className="timeline-event">
+      <div className={"timeline-date" + type}>
+        {date}
+      </div>
+      <div className="timeline-content ">
+        <h3 className="timeline-title">{title}</h3>
+        <p className="timeline-description">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+const Timeline: React.FC<ITimelineProps> = ({ events }) => {
+  return (
+    <div className="timeline">
+      {events.map((event, index) => (
+        <TimelineEvent key={index} date={event.date} title={event.title} description={event.description} />
+      ))}
+    </div>
+  );
+};
+
+export default Timeline;
